b59bdd10236df8c2f62a0e5c5ed2be07a16694dc
GiggleFizz <wizz_fizz77@hotmail.com>
2025-06-08 23:28:24 +1000

    report completed, arguments started

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 from stats import get_word_count
 from stats import get_character_count
+from stats import letter_sort
 
 book_path = "./books/frankenstein.txt"
 
 def main():      
-    book_content = get_book_text(book_path)
     word_count = get_word_count(get_book_text(book_path))
     letter_counts = get_character_count(get_book_text(book_path))
-    print(f"{word_count} words found in the document")
-    print(letter_counts)
-    
+    sorted_list = letter_sort(letter_counts)
+    #print(f"{word_count} words found in the document")
+    #print(letter_counts)
+    #print(letter_list)
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    for item in sorted_list:
+        alpha = item["char"]
+        if alpha.isalpha():
+            print(f"{alpha}: {item["num"]}")
+    print("============= END ===============")
     
 def get_book_text(book_path):
     with open(book_path) as f:
--- stats.py
+++ stats.py
@@ -8,10 +8,20 @@ def get_word_count(book):
 def get_character_count(book):
     lowered_letters = book.lower()
     letter_counts = {}
-    for letter in lowered_letters:
-        if letter in letter_counts:
-            letter_counts[letter] += 1
+    for char in lowered_letters:
+        if char in letter_counts:
+            letter_counts[char] += 1
         else:
-            letter_counts[letter] = 1    
-
+            letter_counts[char] = 1    
     return letter_counts
+
+def sort_on(dict):
+    return dict["num"]
+
+def letter_sort(letter_counts):
+    sorted_list = []
+    for char,count in letter_counts.items():
+        sorted_list.append({"char": char,"num": count})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
